037928a5dc2aeb9124b6ff3ea84fc4d99f7350d3
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-09 11:40:30 +0100

    [ADD] include/Core.hpp

---
+++ include/Core.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2026
+** CORE
+** File description:
+** nanotekspice
+*/
+
+
+#ifndef CORE
+    #define CORE
+    #define SUCCESS 0
+    #define HELP 2026
+    #define ERROR 84
+    #define FAIL 1
+
+#endif /* CORE */
